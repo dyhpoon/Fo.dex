057a62ee62d0aa32d98156aa2de597888b8adf10
Darren Poon <dyhpoon@gmail.com>
2015-01-11 21:12:19 +0800

    re-add navigation drawer, use custom toolbar as actionbar.

--- app/src/main/java/com/dyhpoon/fodex/MainActivity.java
+++ app/src/main/java/com/dyhpoon/fodex/MainActivity.java
@@ -1,11 +1,11 @@
 package com.dyhpoon.fodex;
 
-import android.app.Activity;
-
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.widget.DrawerLayout;
@@ -24,25 +24,45 @@ import java.util.Arrays;
 import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
     /**
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
      */
     private CharSequence mTitle;
+    private Toolbar mToolbar;
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+    private Fragment firstFragment, secondFragment, thirdFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(mToolbar);
+
         mTitle = getTitle();
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
 
         this.setupImageLoader();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            getMenuInflater().inflate(R.menu.main, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
     public void restoreActionBar() {
-        ActionBar actionBar = getActionBar();
+        ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowTitleEnabled(true);
         actionBar.setTitle(mTitle);
     }
@@ -84,6 +104,34 @@ public class MainActivity extends Activity {
                 new PageItem(getString(R.string.title_recent_photos), RecentPhotosPageFragment.class),
                 new PageItem(getString(R.string.title_downloaded_photos), DownloadedPhotosPageFragment.class)
         );
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        Fragment fragment = null;
+        try {
+            fragment = (Fragment) getPages().get(position).getFragmentClass().newInstance();
+            if (position == 0) {
+                firstFragment = fragment;
+            } else if (position == 1) {
+                secondFragment = fragment;
+            } else {
+                thirdFragment = fragment;
+            }
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container, fragment)
+                    .commit();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onSectionAttached(int number) {
+        mTitle = getPages().get(number - 1).getTitle();
     }
 
 }
