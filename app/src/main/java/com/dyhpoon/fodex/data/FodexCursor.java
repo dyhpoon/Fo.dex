bf3f708b3d1b9401962a767e0b7603d3ff75c45b
Darren Poon <dyhpoon@gmail.com>
2015-02-08 17:26:04 +0800

    Add function to get thumbnail's url with image id.

--- app/src/main/java/com/dyhpoon/fodex/data/FodexCursor.java
+++ app/src/main/java/com/dyhpoon/fodex/data/FodexCursor.java
@@ -1,8 +1,10 @@
 package com.dyhpoon.fodex.data;
 
 import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.provider.MediaStore;
 
 /**
@@ -25,6 +27,28 @@ public class FodexCursor {
                 MediaStore.Images.Media.DATE_ADDED + " DESC"
         );
         return cursor;
+    }
+
+    public static Uri getThumbnail(Context context, int imageId) {
+        ContentResolver resolver = context.getContentResolver();
+        String[] projection = new String[] {
+                MediaStore.Images.Thumbnails._ID
+        };
+        Cursor cursor = MediaStore.Images.Thumbnails.queryMiniThumbnail(
+                resolver,
+                imageId,
+                MediaStore.Images.Thumbnails.MINI_KIND,
+                projection);
+        final int idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Thumbnails._ID);
+        if (cursor.moveToFirst()) {
+            final String id = cursor.getString(idColumn);
+            cursor.close();
+            return ContentUris.withAppendedId(
+                    MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
+                    Long.parseLong(id));
+        } else {
+            return null;
+        }
     }
 
 }
