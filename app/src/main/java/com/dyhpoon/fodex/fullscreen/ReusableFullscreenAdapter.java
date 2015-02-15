133dfe7910565b7c970bc6fe88f1616e1ab4de70
Darren Poon <dyhpoon@gmail.com>
2015-02-15 18:37:51 +0800

    Fix fullscreen images look blurry because of images are scaled down.

--- app/src/main/java/com/dyhpoon/fodex/fullscreen/ReusableFullscreenAdapter.java
+++ app/src/main/java/com/dyhpoon/fodex/fullscreen/ReusableFullscreenAdapter.java
@@ -1,21 +1,21 @@
 package com.dyhpoon.fodex.fullscreen;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.support.v4.view.PagerAdapter;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.Priority;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
-import com.bumptech.glide.request.animation.GlideAnimation;
-import com.bumptech.glide.request.target.SimpleTarget;
 import com.dyhpoon.fodex.data.FodexImageContract;
 import com.dyhpoon.fodex.view.TouchImageView;
 
+import java.io.FileNotFoundException;
 import java.util.Stack;
 
 
@@ -29,10 +29,14 @@ public abstract class ReusableFullscreenAdapter extends PagerAdapter {
     public abstract Uri imageUriAtPosition(int position);
 
     private Context mContext;
-    private Stack<View> mRecycledViews = new Stack<View>();
+    private int mWidth, mHeight;
+    private Stack<View> mRecycledViews = new Stack<>();
 
     public ReusableFullscreenAdapter(Context context) {
         mContext = context;
+        DisplayMetrics metrics = mContext.getResources().getDisplayMetrics();
+        mWidth = metrics.widthPixels;
+        mHeight = metrics.heightPixels;
     }
 
     @Override
@@ -48,18 +52,54 @@ public abstract class ReusableFullscreenAdapter extends PagerAdapter {
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
         final ImageView photoView = (ImageView) createOrRecycleView(mContext);
-        Glide.with(mContext)
-                .loadFromMediaStore(imageUriAtPosition(position))
-                .priority(Priority.IMMEDIATE)
-                .thumbnail(0.3f)
-                .into(new SimpleTarget<GlideDrawable>() {
-                    @Override
-                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
-                        photoView.setImageDrawable(resource);
-                    }
-                });
+        Bitmap bm = decodeSampledBitmapFromResource(imageUriAtPosition(position), mWidth, mHeight);
+        photoView.setImageBitmap(bm);
         container.addView(photoView, 0);
         return photoView;
+    }
+
+    private Bitmap decodeSampledBitmapFromResource(Uri uri, int reqWidth, int reqHeight) {
+        try {
+            // First decode with inJustDecodeBounds=true to check dimensions
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            AssetFileDescriptor fd =
+                    mContext.getContentResolver().openAssetFileDescriptor(uri, "r");
+            BitmapFactory.decodeFileDescriptor(fd.getFileDescriptor(), null, options);
+
+            // Calculate inSampleSize
+            options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+            // Decode bitmap with inSampleSize set
+            options.inJustDecodeBounds = false;
+            return BitmapFactory.decodeFileDescriptor(fd.getFileDescriptor(), null, options);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 
     @Override
@@ -77,7 +117,7 @@ public abstract class ReusableFullscreenAdapter extends PagerAdapter {
             photoView.setBackgroundColor(Color.BLACK);
             photoView.setLayoutParams(new ViewGroup.LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT
+                    ViewGroup.LayoutParams.MATCH_PARENT
             ));
             photoView.setMinimumHeight(FodexImageContract.preferredMinimumHeight(mContext));
         } else {
