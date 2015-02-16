76b2aaff58e71f2b5b393ac1f1b94658a9970844
Darren Poon <dyhpoon@gmail.com>
2015-02-16 19:56:45 +0800

    Attempt to fix OutOfMemory exception.

--- app/src/main/java/com/dyhpoon/fodex/util/MediaImage.java
+++ app/src/main/java/com/dyhpoon/fodex/util/MediaImage.java
@@ -31,6 +31,10 @@ public class MediaImage {
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+        } catch (OutOfMemoryError e) {
+            // When receive OOM, call GC, and try to get a smaller size of image.
+            System.gc();
+            return getDecodedBitmap(context, uri, reqWidth/2, reqHeight/2);
         }
         return null;
     }
@@ -43,13 +47,10 @@ public class MediaImage {
 
         if (height > reqHeight || width > reqWidth) {
 
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
             // Calculate the largest inSampleSize value that is a power of 2 and keeps both
             // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) > reqHeight
-                    && (halfWidth / inSampleSize) > reqWidth) {
+            while ((height / inSampleSize) > reqHeight
+                    || (width / inSampleSize) > reqWidth) {
                 inSampleSize *= 2;
             }
         }
