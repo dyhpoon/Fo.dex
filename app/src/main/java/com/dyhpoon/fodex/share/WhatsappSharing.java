9334884e34796147cfe35141e4b43b6af0e7706a
Darren Poon <dyhpoon@gmail.com>
2015-03-19 21:36:25 +0800

    Fix duplicated image when sharing to whatsapp.

--- app/src/main/java/com/dyhpoon/fodex/share/WhatsappSharing.java
+++ app/src/main/java/com/dyhpoon/fodex/share/WhatsappSharing.java
@@ -2,18 +2,9 @@ package com.dyhpoon.fodex.share;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Environment;
-import android.util.DisplayMetrics;
 
-import com.dyhpoon.fodex.util.MediaImage;
 import com.dyhpoon.fodex.util.OnCompleteListener;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 /**
  * Created by darrenpoon on 16/3/15.
@@ -24,26 +15,10 @@ public class WhatsappSharing extends Sharing {
 
     @Override
     public void shareImage(Context context, Uri uri, OnCompleteListener listener) {
-        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-        int width = metrics.widthPixels;
-        int height = metrics.heightPixels;
-
-        Bitmap bitmap = MediaImage.getDecodedBitmap(context, uri, width, height);
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("image/jpeg");
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        File f = new File(Environment.getExternalStorageDirectory() + File.separator + "temporary_file.jpg");
 
-        try {
-            f.createNewFile();
-            new FileOutputStream(f).write(bytes.toByteArray());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        shareIntent.putExtra(Intent.EXTRA_STREAM,
-                Uri.parse(Environment.getExternalStorageDirectory() + File.separator + "temporary_file.jpg"));
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
         if (isPackageInstalled(WHATSAPP_PACKAGE, context)) {
             listener.didComplete();
             shareIntent.setPackage(WHATSAPP_PACKAGE);
