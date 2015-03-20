fbbf6f3e0138ca73049b997167d69be64a5129ec
Darren Poon <dyhpoon@gmail.com>
2015-03-20 22:20:28 +0800

    Add Facebook sharing. Replace a new facebook sharing icon.

---
+++ app/src/main/java/com/dyhpoon/fodex/share/FacebookSharing.java
@@ -0,0 +1,36 @@
+package com.dyhpoon.fodex.share;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import com.dyhpoon.fodex.R;
+import com.dyhpoon.fodex.util.OnCompleteListener;
+
+/**
+ * Created by darrenpoon on 20/3/15.
+ */
+public class FacebookSharing extends Sharing {
+
+    private static final String FACEBOOK_PACKAGE = "com.facebook.katana";
+
+    @Override
+    public void shareImage(Context context, Uri uri, OnCompleteListener listener) {
+        if (isPackageInstalled(FACEBOOK_PACKAGE, context)) {
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.setType("image/*");
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+
+            listener.didComplete();
+            shareIntent.setPackage(FACEBOOK_PACKAGE);
+            shareIntent.putExtra(
+                    "com.facebook.platform.extra.APPLICATION_ID",
+                    context.getResources().getString(R.string.facebook_app_id));
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            context.startActivity(Intent.createChooser(shareIntent, "Share"));
+        } else {
+            listener.didFail();
+        }
+    }
+}
